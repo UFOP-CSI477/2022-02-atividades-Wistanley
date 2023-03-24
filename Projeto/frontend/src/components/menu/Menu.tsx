fc20a239fb5e7486cfc8ac6834b767aec1be0604
Wistanley <wistanleyoliveira@gmail.com>
2023-03-23 21:50:40 -0300

    Alterações no projeto

--- Projeto/frontend/src/components/menu/Menu.tsx
+++ Projeto/frontend/src/components/menu/Menu.tsx
@@ -11,7 +11,7 @@ const Menu = () => {
             <ul>
             <img src="https://cdn-icons-png.flaticon.com/512/6889/6889345.png" style={{ width: "50px" }}/>
                 <li><Link to='/'>Home</Link></li>
-                <li><Link to='/estados'>Cotações</Link></li>
+                <li><Link to='/cotacoes'>Cotações</Link></li>
                 <li><Link to='/cidades'>Minha Conta</Link></li>
                 <li><Link to='/pessoas'>Serviços Disponíveis</Link></li>
             </ul>
